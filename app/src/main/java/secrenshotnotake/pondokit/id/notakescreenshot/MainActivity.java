5ad2c46349e70a4e1e3d5274efbc7b582df13dfe
Kakzam.zam <adzan.zam@gmail.com>
2019-05-09 10:02:12 +0700

    Semoga Bermanfaat Dan Di Beri Berkah

---
+++ app/src/main/java/secrenshotnotake/pondokit/id/notakescreenshot/MainActivity.java
@@ -0,0 +1,15 @@
+package secrenshotnotake.pondokit.id.notakescreenshot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+    }
+}
